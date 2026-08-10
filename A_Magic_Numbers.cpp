089d4b8ad63013f2d9a5f832d6910d950f0d1a62
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-08-10 22:36:01 +0530

    320A

---
+++ A_Magic_Numbers.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    while(n!=0){
+        if(n%10==1){
+            n/=10;
+        }else if(n%100==14){
+            n/=100;
+        }else if(n%1000==144){
+            n/=1000;
+        }else{
+            cout<<"NO"<<endl;
+            return 0;
+        }
+    }
+    cout<<"YES"<<endl;
+    return 0;
+}
