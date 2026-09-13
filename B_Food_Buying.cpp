3d746c103dd367d33bfd3f0eb3030b0793778468
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-09-13 16:40:06 +0530

    1296B

---
+++ B_Food_Buying.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long n;
+        cin>>n;
+
+        long long ans=0;
+
+        
+        ans=(n+(n-1)/9);
+        cout<<ans<<endl;
+    }
+}
